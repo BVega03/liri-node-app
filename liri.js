ec320d36dbe8f664c444bd0e00a07789ea877fc0
Benny V <bengv2303@gmail.com>
2019-01-03 14:08:55 -0700

    updated spotify & omdb data

--- liri.js
+++ liri.js
@@ -5,8 +5,8 @@ var moment = require("moment");
 var keys = require("./keys");
 var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
-var movie = process.argv[2];
-var concertSearch = process.argv[2];
+var command = process.argv[2];
+var searchTerm = process.argv.splice(3);
 
 // User types in node liri.js and one of these commands
 // 1. concert-this 
@@ -17,62 +17,92 @@ var concertSearch = process.argv[2];
 // Based on the command typed, AJAX request will return and print out the return AJAX request in the terminal
 // Based on the command, we can do a "switch" statement
 
-var command = process.argv[2];
-var searchTerm = process.argv.splice(3);
+
 
 switch (command) {
-    case "concert-this":
-        concertSearch();
-        break;
+  case "concert-this":
+    concertSearch();
+    break;
 
-    case "spotify-this-song":
-        spotifySearch();
-        break;
+  case "spotify-this-song":
+    spotifySearch();
+    break;
 
-    case "movie-this":
-        movie();
-        break;
+  case "movie-this":
+    movieSearch();
+    break;
 
-    case "do-what-it-says":
-        thisWay();
-        break;
+  case "do-what-it-says":
+    thisWay();
+    break;
 }
 
 function concertSearch() {
-    console.log("this is the concert search function");
+  console.log("this is the concert search function");
 
-    axios.get('https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp')
-  .then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+  axios.get('https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp')
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 };
 
 function spotifySearch() {
-    console.log("this is the Spotify search function");
-    spotify.search({ type: 'track', query: searchTerm }, function(err, data) {
-        if (err) {
-          return console.log('Error occurred: ' + err);
-        }
-       
-      console.log(data); 
-      });
+  console.log("this is the Spotify search function");
+  spotify.search({
+    type: 'track',
+    query: searchTerm
+  }, function (err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+
+    // console.log(data.tracks.items); 
+
+    for (var i = 0; i < 3; i++) {
+
+      console.log("Song Name " + data.tracks.items[i].name);
+      console.log("Artist " + data.tracks.items[i].artists[0].name);
+      console.log("Album " + data.tracks.items[i].album.name);
+      console.log("Link to Album " + data.tracks.items[i].preview_url);
+
+    }
+
+  });
 };
 
-function movie() {
-    console.log("this is the movie search function");
-    axios.get(' http://www.omdbapi.com/?i=tt3896198&apikey=1a9da948')
-  .then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+function movieSearch() {
+  console.log(searchTerm);
+  axios.get('http://www.omdbapi.com/?t=' + searchTerm + '&apikey=1a9da948')
+    .then(function (response) {
+      console.log(response);
+      console.log("Title " + data.Title);
+      // console.log("Year " + data.items[i].Year);
+      // console.log("IMDB Rating " + data.items[i].imdbRating);
+      // console.log("Rotten Tomatoes Rating " + data.items[i].Ratings);
+      // console.log("Country " + data.items[i].Country);
+      // console.log("Language " + data.items[i].Language);
+      // console.log("Plot " + data.items[i].Plot);
+      // console.log("Actors " + data.items[i].Actors);
+
+
+      // * Title of the movie.
+      // * Year the movie came out.
+      // * IMDB Rating of the movie.
+      // * Rotten Tomatoes Rating of the movie.
+      // * Country where the movie was produced.
+      // * Language of the movie.
+      // * Plot of the movie.
+      // * Actors in the movie.
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 };
 
 
 function thisWay() {
-    console.log("this is the do it this way search function");
+  console.log("this is the do it this way search function");
 };
